b0ed4e22e3f27fccbf6804c4c55dc55a837c1374
um-31 <udaymahajan97@gmail.com>
2019-06-13 19:54:03 -0400

    [Udhay] further

--- CollegeSystem/main.swift
+++ CollegeSystem/main.swift
@@ -8,9 +8,24 @@
 
 import Foundation
 
-var p = Person()
-var s = Student()
+var p1 = Person()
+var p2 = Person()
+var p3 = Person()
+var p4 = Person()
+var p5 = Person()
+var p6 = Person()
+var p7 = Person()
+var s1 = Student()
+var s2 = Student()
+var s3 = Student()
+var s4 = Student()
+var s5 = Student()
+var e1 = Employee()
+var e2 = Employee()
+var e3 = Employee()
+
 var studentAddress = Address(suiteNumber: 64, streetName: "Bedle Ave", city: "Toronto", postalCode: "M2H 1K8", country: "Canada")
+var students: Dictionary<>
 
 s.address = studentAddress
 s.id = 100
